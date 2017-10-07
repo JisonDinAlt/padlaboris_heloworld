4091148b17b5397f4bfdffd856fe6c3eee1dbfe8
emercom <emercom@grodno.net>
2017-10-07 09:25:53 +0300

    initial commit

---
+++ src/main/java/com/example/padlaboris/demo.java
@@ -0,0 +1,16 @@
+package com.example.padlaboris;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+    @RequestMapping (value = "/",method = RequestMethod.GET)
+    public ResponseEntity hello(){
+
+        return ResponseEntity.ok().body("Hello New Brave World");
+    }
+}
